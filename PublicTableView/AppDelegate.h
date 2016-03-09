0dab417fdff350957c5b29fc7100b4971f2c2cce
shenyuan <shenyuan@mitake.com.cn>
2016-03-09 17:19:34 +0800

    Initial Commit

---
+++ PublicTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PublicTableView
+//
+//  Created by shenyuan on 16/3/9.
+//  Copyright © 2016年 Mitake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PublicTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PublicTableView
+//
+//  Created by shenyuan on 16/3/9.
+//  Copyright © 2016年 Mitake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
